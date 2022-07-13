a80d7c3b5a097a07f1c17f3776d12183fe8fc7c1
David14gb <davidgarciab14@gmail.com>
2022-07-13 13:21:08 +0200

    cambio en distancia por id_usuario

--- src/controller/pipicanes.controller.js
+++ src/controller/pipicanes.controller.js
@@ -2,7 +2,7 @@ const { request } = require("express");
 const connection =require("../database")
 function getAllPipi(request,response)
 {
-    let sql = "SELECT * FROM pipican ORDER BY distancia ASC LIMIT 5 ";
+    let sql = "SELECT * FROM pipican ORDER BY id_pipican ASC LIMIT 5 ";
     
 
     connection.query(sql, (err, result) => {
