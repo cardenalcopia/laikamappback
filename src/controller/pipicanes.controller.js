9512de10ed814085b12155f11fc7c258156e9e28
David14gb <davidgarciab14@gmail.com>
2022-07-17 13:25:52 +0200

    subida rating

--- src/controller/pipicanes.controller.js
+++ src/controller/pipicanes.controller.js
@@ -17,8 +17,10 @@ function getAllPipi(request,response)
 }
 function getOnePipi(request,response)
 {
-    let sql =  "SELECT * FROM pipican WHERE id_pipican='" + request.query.id_pipican +"'";
-
+    
+    // let sql =  "SELECT * FROM pipican WHERE id_pipican='" + request.query.id_pipican +"'";
+    let sql = 'SELECT AVG(calificacion) AS marks FROM rating WHERE id_pipican =' + request.query.id_pipican
+    
     connection.query(sql, (err, result) => {
         if(err){
             console.log(err);
@@ -46,4 +48,67 @@ function getMapaPipi(request,response)
 
 }
 
-module.exports={getAllPipi,getOnePipi, getMapaPipi}
+function postRating(request, response){
+
+    let sql = "INSERT INTO rating (id_pipican, calificacion, id_usuario)" + 
+    "VALUES ('" + request.body.id_pipican + "', '" +
+                    request.body.calificacion + "', '" +
+                    request.body.id_usuario + "')"; 
+                    console.log(sql);
+                    connection.query(sql, (err, result) => {
+                        if( err ){
+                            console.log( err );
+                        }
+                        else{
+                            console.log(result);
+                            if(result){
+                                response.send(result);
+                            }
+                            else{
+                                response.send(result);
+                            }
+                        }
+                    })
+}
+
+function getAvg(request, response){
+    
+    let id = request.query.id_pipican
+
+    let sql = 'SELECT AVG(calificacion) FROM rating WHERE id_pipican =' + id
+
+    console.log(sql);
+
+    connection.query(sql, (err, result) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            response.send(result);
+        }
+    });
+}
+
+function putRating(request, response){
+
+    let id_pipican = request.body.id_pipican ? request.body.id_pipican : null;
+    let rating = request.body.rating ? request.body.rating : null;
+    console.log(id_pipican);
+    console.log(rating);
+   
+    let params =[rating,id_pipican]
+    let sql = "UPDATE pipican SET pipican.rating = COALESCE(?, pipican.rating) WHERE id_pipican = ?";
+    console.log(sql); 
+    connection.query(sql, params,function (err, result) 
+    {
+        if (err) 
+            console.log(err);
+        else 
+        {
+            console.log("Pipican modificado");
+            response.send(result);
+        }
+    })
+
+}
+module.exports={getAllPipi,getOnePipi, getMapaPipi, postRating, getAvg, putRating}
--- src/routers/pipicanes.router.js
+++ src/routers/pipicanes.router.js
@@ -4,5 +4,8 @@ const pipiCtrl = require("../controller/pipicanes.controller")
 router.get("/cards-pipicanes",pipiCtrl.getAllPipi)
 router.get("/pipicanCard",pipiCtrl.getOnePipi)
 router.get("/home",pipiCtrl.getMapaPipi)
+router.post("/pipicanCard",pipiCtrl.postRating)
+router.get("/pipicanCard",pipiCtrl.getAvg)
+router.put("/pipicanCard",pipiCtrl.putRating)
 
 module.exports=router;
